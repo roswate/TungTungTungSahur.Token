21d947309c5869e9e6d74458e21a28e5b22f4b88
roswate <bratzgulsu@gmail.com>
2025-05-16 01:27:00 +0300

    Update TTS.sol
    HERE WE ARE

--- contracts/TTS.sol
+++ contracts/TTS.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TungTungTungSahurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TungTungTungSahurToken", "TTS") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+}
